800f33a9ff073fd6d4d0a5efd4719de6045b8430
kirbodev <superkirbybiz@email.com>
2023-11-12 21:22:54 +0000

    Should've fixed the build issues

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -1,5 +1,15 @@
 import pino from 'pino';
 import logCleanup from './logCleanup';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+
+// Check if logs directory exists and create it if it doesn't
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const logDir = path.join(__dirname, '../../logs');
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
 
 const transport = pino.transport({
     targets: [
@@ -8,8 +18,7 @@ const transport = pino.transport({
             target: 'pino/file',
             options: {
                 // ISO string is an invalid file name so replace colons with dashes
-                destination: `./logs/LOG-${new Date(Date.now()).toISOString().replace(/:/g, '-')}.log`,
-                mkdir: true
+                destination: `./logs/LOG-${new Date(Date.now()).toISOString().replace(/:/g, '-')}.log`
             },
         },
         {
