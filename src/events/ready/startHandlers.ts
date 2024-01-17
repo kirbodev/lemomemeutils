8c6d114fedaa57cbd21c6a2199a2f3b408f5796b
kirbodev <superkirbybiz@email.com>
2024-01-17 17:42:59 +0000

    emergency fix (hopefully?)

--- src/events/ready/startHandlers.ts
+++ src/events/ready/startHandlers.ts
@@ -1,7 +1,7 @@
 import type { Client } from "discord.js";
 import jobHandler from "../../handlers/jobHandler";
 
-export default async (client: Client) => {
+export default (client: Client) => {
     // Event handler is obviously not here because it needs to trigger this event
-    await jobHandler(client);
+    jobHandler(client);
 }
