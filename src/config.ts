c2e72b828031644deeffc5d99ddff07b4582c275
kirbodev <superkirbybiz@email.com>
2024-02-12 12:15:26 +0000

    Removed dankus from dev list

--- src/config.ts
+++ src/config.ts
@@ -20,23 +20,27 @@ import { StaffLevel } from "./structures/staffInterface";
 
 const config = new Collection<string, Config>();
 // Test server
-config.set("907340495498407977", new Config(
-    {
-        guildID: "907340495498407977",
-        firstWarnRoleID: "1172919578641317939",
-        secondWarnRoleID: "1172920021371072534",
-        paroleRoleID: "1180542522062278707",
-        prefix: ",",
-        thinIceRoleID: "1187137588813893692",
-        thinnerIceRoleID: "1187137637841113179",
-        logChannelID: "1036213847293444166",
-        staffApplicationsChannelID: "1195120979039502377",
-        staffVoteChannelID: "1011952861325819984",
-        staffVoteRoles: ["1191713864274939914"],
-        staffRoles: [null, null, "907341095145472010"]}
-));
+config.set(
+  "907340495498407977",
+  new Config({
+    guildID: "907340495498407977",
+    firstWarnRoleID: "1172919578641317939",
+    secondWarnRoleID: "1172920021371072534",
+    paroleRoleID: "1180542522062278707",
+    prefix: ",",
+    thinIceRoleID: "1187137588813893692",
+    thinnerIceRoleID: "1187137637841113179",
+    logChannelID: "1036213847293444166",
+    staffApplicationsChannelID: "1195120979039502377",
+    staffVoteChannelID: "1011952861325819984",
+    staffVoteRoles: ["1191713864274939914"],
+    staffRoles: [null, null, "907341095145472010"],
+  })
+);
 // Lemomeme
-config.set("538903170189885460", new Config({
+config.set(
+  "538903170189885460",
+  new Config({
     guildID: "538903170189885460",
     firstWarnRoleID: "724839115828232232",
     secondWarnRoleID: "724839112192032798",
@@ -48,16 +52,36 @@ config.set("538903170189885460", new Config({
     staffApplicationsChannelID: "1096120117974290503",
     staffAppRoleID: "575811748284858368",
     staffVoteChannelID: "1096121574983225435",
-    staffVoteRoles: ["594576990212849767", "553354789359058954", "1124428178102948011"],
-    staffRoles: [null, "688607097948078092", "554006029365542922", "553354935425564713", "555585458281775115", "553354789359058954", "1124428178102948011", "594576990212849767"],
+    staffVoteRoles: [
+      "594576990212849767",
+      "553354789359058954",
+      "1124428178102948011",
+    ],
+    staffRoles: [
+      null,
+      "688607097948078092",
+      "554006029365542922",
+      "553354935425564713",
+      "555585458281775115",
+      "553354789359058954",
+      "1124428178102948011",
+      "594576990212849767",
+    ],
     linkedStaffRoles: new Map([
-        [StaffLevel.Farmer, "555134155395039256"], // Staff role
-        [StaffLevel.Pineapple, "1162007763426234459"] // High staff role
-    ])
-}));
+      [StaffLevel.Farmer, "555134155395039256"], // Staff role
+      [StaffLevel.Pineapple, "1162007763426234459"], // High staff role
+    ]),
+  })
+);
 
 export default config;
 export let maintainanceMode = process.env.NODE_ENV ? true : false;
-export const toggleMode = () => maintainanceMode = !maintainanceMode;
-export const devs = ["980280857958965328", "695228246966534255", "923993065784508496", "884036644234231819", "551032697578061858", "230122719511969792"];
+export const toggleMode = () => (maintainanceMode = !maintainanceMode);
+export const devs = [
+  "980280857958965328",
+  "695228246966534255",
+  "884036644234231819",
+  "551032697578061858",
+  "230122719511969792",
+];
 export const testServer = "907340495498407977";
