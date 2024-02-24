4bddeb2b27cfa2e4b8e240737c81e1942d3df6e7
kirbodev <superkirbybiz@email.com>
2024-02-24 21:07:11 +0000

    changelog

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,14 +1,9 @@
 export default {
-  name: "Bug Fixes + Small Feature Update",
-  date: new Date(2024, 1, 16),
-  version: "1.5.0",
-  description: "Fixed some bugs and added a QR code filtering system.",
-  changelog: [
-    "Fixed a bug where all staff could use unban.",
-    "Changed the way staff applications are handled.",
-    "Added a delete_messages option to the ban slash command.",
-    "Added a QR code filtering system to the bot.",
-  ],
+  name: "HOV Release",
+  date: new Date(2024, 1, 24),
+  version: "1.5.1",
+  description: "Release for House of VOX.",
+  changelog: ["Changed config and different features to work with HOV."],
 } satisfies {
   name: string;
   date: Date;
