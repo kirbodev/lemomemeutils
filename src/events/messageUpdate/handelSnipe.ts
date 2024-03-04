e57ffc670293471437a9b6d6caad2ad814c2319c
Dunkelrot <support@dunkelrot.xyz>
2024-03-04 11:22:08 -0500

    Update handelSnipe.ts

--- src/events/messageUpdate/handelSnipe.ts
+++ src/events/messageUpdate/handelSnipe.ts
@@ -1,1 +1,22 @@
-// Placeholder
+// src/events/messageUpdate.ts
+import { Client, Message } from "discord.js";
+import SnipedMessage from "../../db/models/snipe";
+export default async (client: Client, oldMessage: Message, newMessage: Message) => {
+  if (!newMessage.guild) return;
+  if (newMessage.partial) await newMessage.fetch().catch(() => null); // Fetch if the message is partial
+  // Ignore if there's no change in content or if the message now has no content
+  if (oldMessage.content === newMessage.content || !newMessage.content) return;
+
+  const snipedMessage = new SnipedMessage({
+    messageId: newMessage.id,
+    authorId: newMessage.author.id,
+    guildId: newMessage.guild.id,
+    channelId: newMessage.channel.id,
+    methodType: "edit",
+    content: newMessage.content,
+    hasMedia: newMessage.attachments.size > 0,
+    timestamp: new Date(),
+  });
+
+  await snipedMessage.save();
+};
