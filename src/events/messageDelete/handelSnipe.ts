6da9552bd353662c12ce60564bc827ea21570fad
Dunkelrot <support@dunkelrot.xyz>
2024-03-04 11:22:47 -0500

    Update handelSnipe.ts

--- src/events/messageDelete/handelSnipe.ts
+++ src/events/messageDelete/handelSnipe.ts
@@ -1,1 +1,21 @@
-// Placeholder
+import { Client, Message } from "discord.js";
+import SnipedMessage from "../../db/models/snipe";
+
+export default async (client: Client, message: Message) => {
+  if (!message.guild) return;
+  if (message.partial) await message.fetch().catch(() => null); // Fetch if the message is partial
+  if (!message.content && !message.attachments.size) return; // Ignore if there's no content or attachments
+
+  const snipedMessage = new SnipedMessage({
+    messageId: message.id,
+    authorId: message.author.id,
+    guildId: message.guild.id,
+    channelId: message.channel.id,
+    methodType: "delete",
+    content: message.content,
+    hasMedia: message.attachments.size > 0,
+    timestamp: new Date(),
+  });
+
+  await snipedMessage.save();
+};
