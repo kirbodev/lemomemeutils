3cf268ef6dbb50a0c5388ee34eb092e2d1a162e4
Dunkelrot <support@dunkelrot.xyz>
2024-03-04 12:05:24 -0500

    Update changelog.ts

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,12 +1,12 @@
 export default {
-  name: "Changes to the AFK Command",
-  date: new Date(2024, 2, 2),
-  version: "1.5.5",
+  name: "New Command and a fraction of an AutoMod!",
+  date: new Date(2024, 2, 4),
+  version: "1.5.6",
   description: "3 day update streak 🔥! ",
   changelog: [
     "We received some feedback about the AFK command and made some changes to it.",
-    "The AFK command now supports attachments. Either use the `attachment` option on </afk:1213262685450276944> or attach a file when using `,afk`.",
-    "Made the anti-dyno advisory message more clear.",
+    "Added the `snipe` command (Slash only) with edit and deleted ",
+    "Adding malicious link prevention with support from Total Virus to automatically detect malicious links and delete them, sending staff a notification to investigate.",
   ],
 } satisfies {
   name: string;
