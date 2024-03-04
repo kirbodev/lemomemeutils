dfdca4e571c999c88d0dbd3a9ee30de4be5418de
Dunkelrot <support@dunkelrot.xyz>
2024-03-04 11:14:36 -0500

    Create snipe.ts

---
+++ src/db/models/snipe.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import snipeInterface from "../../structures/snipeInterface";
+
+const snipeSchema = new mongoose.Schema<snipeInterface>({
+  messageId: {
+    type: String,
+    required: true,
+  },
+  authorId: {
+    type: String,
+    required: true,
+  },
+  guildId: {
+    type: String,
+    required: true,
+  },
+  channelId: {
+    type: String,
+    required: false,
+  },
+  methodType: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  hasMedia: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export default mongoose.models.snipe || mongoose.model<snipeInterface>("snipe", snipeSchema, "snipes");
