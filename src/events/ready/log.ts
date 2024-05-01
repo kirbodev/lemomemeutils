844b970307d10d300b8d8dde4f34266bb7616b9a
kdv_ <luca@kdv.one>
2024-05-01 22:16:00 +0100

    forgot to remove test

--- src/events/ready/log.ts
+++ src/events/ready/log.ts
@@ -4,12 +4,4 @@ import logger from "../../helpers/logger.js";
 export default async (client: Client) => {
   logger.info(`Logged in as ${client.user?.username}!`);
   process.stdout.write("readySignal");
-
-  const now = performance.now();
-  const c = await client.channels.fetch("1011953546062745620");
-  const time = performance.now() - now;
-  c?.isTextBased() && c.send(`heartbeat ${Date.now() - time} ${process.pid}`);
-  setInterval(async () => {
-    c?.isTextBased() && c.send(`heartbeat ${Date.now()} ${process.pid}`);
-  }, 1000);
 };
