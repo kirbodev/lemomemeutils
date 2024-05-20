6b477bc0064c187b028dfbba5c0d1406d6a7f825
kirbo dev <luca@kdv.one>
2024-05-20 21:48:53 +0100

    v1.8.4beta

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,15 +1,17 @@
 export default {
-  name: "Sentience - Beta2",
+  name: "Sentience - Beta3",
   date: new Date(2024, 4, 18),
-  version: "1.8.3beta",
+  version: "1.8.4beta",
   description:
-    "Sentience has events now.",
+    "Sentience can now use multiple API keys, and will use the one with the lowest usage count.",
   changelog: [
-    "Events are now a thing, they have a 33% chance to happen.",
-    "The bot will tell you if there is an event set.",
-    "Moderate the bot as normal! Some events may produce suggestive results",
-    "Devs can set the event with `@Pomegranate setEvent <event>`",
-    "Updates will be infrequent until after finals 💔"
+    "Events are now server specific",
+    "High staff should be able to set events with `@Pomegranate setEvent <event>`",
+    "Freaky mode has been disabled in HOV",
+    "An experimental multi API key system has been added, which should reduce the chance of rate limiting",
+    "The bot will now use the lowest usage count API key for each user",
+    "Rizz mode has been added",
+    "⚠️ All updates have been halted for about a month, so I can study for finals 🫡",
   ],
 } satisfies {
   name: string;
