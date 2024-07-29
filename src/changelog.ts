e476e301c168e87f7d209b87bc73b1dcdf61fbe6
Dunkelrot <support@dunkelrot.xyz>
2024-07-28 23:04:14 -0400

    Update changelog.ts

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,19 +1,12 @@
 export default {
   name: "Sentience - Beta3",
-  date: new Date(2024, 4, 18),
-  version: "1.8.4beta",
+  date: new Date(2024, 7, 29),
+  version: "1.8.4beta2",
   description:
-    "Sentience can now use multiple API keys, and will use the one with the lowest usage count.",
+    "You can lock names now!",
   changelog: [
-    "Events are now server specific",
-    "High staff should be able to set events with `@Pomegranate setEvent <event>`",
-    "Freaky mode has been disabled in HOV",
-    "An experimental multi API key system has been added, which should reduce the chance of rate limiting",
-    "The bot will now use the lowest usage count API key for each user",
-    "Rizz mode has been added",
-    "Logging from Fruit Harvester will be pretty now",
-    "Small bug fixes",
-    "⚠️ All updates have been halted for about a month, so I can study for finals 🫡",
+    "Added the `namelock` and `unnamelock` commands and any logic required. - dunk",
+    "⚠️ All updates have been halted for about a month, so I can study for finals 🫡 - kirbo",
   ],
 } satisfies {
   name: string;
