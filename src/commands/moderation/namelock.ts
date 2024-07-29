c558f7e835194c1d21832b839c0c11eb4f1d026f
Dunkelrot <support@dunkelrot.xyz>
2024-07-28 23:14:45 -0400

    Update namelock.ts

--- src/commands/moderation/namelock.ts
+++ src/commands/moderation/namelock.ts
@@ -14,7 +14,7 @@ import {
 import EmbedColors from "../../structures/embedColors.js";
 import Errors from "../../structures/errors.js";
 import safeEmbed from "../../utils/safeEmbed.js";
-import { setNameLock, getNameLock } from "../../db/models/namelocks";
+import { setNameLock, getNameLock } from "../../db/models/namelock.ts";
 
 const monitorUsernameChange = async (guildId: string, userId: string, interaction: ChatInputCommandInteraction) => {
   const nameLock = await getNameLock(guildId, userId);
