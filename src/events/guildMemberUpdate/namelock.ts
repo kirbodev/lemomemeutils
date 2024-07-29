3782267b3b1f49fa11474e0f4ad1619fda49b7e9
Dunkelrot <support@dunkelrot.xyz>
2024-07-28 23:17:32 -0400

    Update namelock.ts

--- src/events/guildMemberUpdate/namelock.ts
+++ src/events/guildMemberUpdate/namelock.ts
@@ -1,5 +1,5 @@
 import type { Client, GuildMember } from "discord.js";
-import { getNameLock } from "../../db/models/namelock.ts";
+import { getNameLock } from "../../db/models/namelock";
 import logger from "../../helpers/logger.js";
 
 export default async (client: Client, oldMember: GuildMember, newMember: GuildMember) => {
