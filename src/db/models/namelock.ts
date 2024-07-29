ecf90ef416f9ba267cdfbbdcc97ba4d81327912b
Dunkelrot <support@dunkelrot.xyz>
2024-07-28 22:54:18 -0400

    Create namelock.ts

---
+++ src/db/models/namelock.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Document } from "mongoose";
+
+interface NameLock extends Document {
+  guildId: string;
+  userId: string;
+  lockedName: string;
+}
+
+const NameLockSchema = new Schema<NameLock>({
+  guildId: { type: String, required: true },
+  userId: { type: String, required: true },
+  lockedName: { type: String, required: true },
+});
+
+const NameLockModel = model<NameLock>("NameLock", NameLockSchema);
+
+export const setNameLock = async (guildId: string, userId: string, lockedName: string) => {
+  await NameLockModel.updateOne(
+    { guildId, userId },
+    { $set: { lockedName } },
+    { upsert: true }
+  );
+};
+
+export const getNameLock = async (guildId: string, userId: string) => {
+  return NameLockModel.findOne({ guildId, userId }).exec();
+};
+
+export default NameLockModel;
